2f76201a892e8da6c1a4d6045b893c17f49608e8
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2024-06-30 12:09:43 +0530

    Initial commit

---
+++ src/test/java/com/codelabs/liquibase/LiquibaseExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelabs.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
